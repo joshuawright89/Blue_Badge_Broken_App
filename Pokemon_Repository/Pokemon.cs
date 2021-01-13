21741045b53f3aaba32c6f37a8b39c0a751985d6
joshuawright89 <73801906+joshuawright89@users.noreply.github.com>
2021-01-12 20:23:38 -0500

    Not sure if I cloned this right...

---
+++ Pokemon_Repository/Pokemon.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Repository
+{
+    public enum PokemonType
+    {
+        Normal = 1,
+        Grass,
+        Fire,
+        Water,
+        Electric = 5,
+        Ice,
+        Bug,
+        Ground,
+        Rock,
+        Fighting = 10,
+        Psychic,
+        Ghost,
+        Dark,
+        Fairy,
+        Dragon = 15
+    }
+
+    public enum PokemonSecondaryType
+    {
+        Normal = 1,
+        Grass,
+        Fire,
+        Water,
+        Electric = 5,
+        Ice,
+        Bug,
+        Ground,
+        Rock,
+        Fighting = 10,
+        Psychic,
+        Ghost,
+        Dark,
+        Fairy,
+        Dragon = 15,
+        None = 16
+    }
+    public class Pokemon
+    {
+        public string PokemonSpeciesName { get; set; }
+        public string PokemonNickName { get; set; }
+        public int Level { get; set; }
+        public PokemonType PokemonPrimaryType { get; set; }
+        public PokemonType PokemonSecondaryType { get; set; }
+        public string MoveOne { get; set; }
+        public string MoveTwo { get; set; }
+        public string MoveThree { get; set; }
+        public string MoveFour { get; set; }
+
+        public Pokemon(string pokemonSpeciesName, string pokemonNickName, int level, PokemonType pokemonPrimaryType, PokemonType pokemonSecondaryType, string moveOne, string moveTwo, string moveThree, string moveFour)
+        {
+            //TeamPosition = teamPosition;
+            PokemonSpeciesName = pokemonSpeciesName;
+            PokemonNickName = pokemonNickName;
+            Level = level;
+            PokemonPrimaryType = pokemonPrimaryType;
+            PokemonSecondaryType = pokemonSecondaryType;
+            MoveOne = moveOne;
+            MoveTwo = moveTwo;
+            MoveThree = moveThree;
+            MoveFour = moveFour;
+        }
+
+        public Pokemon() { }
+    }
+}
---
+++ Pokemon_Repository/Pokemon_Repository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Repository
+{
+    public class PokemonRepository
+    {
+        public List<Pokemon> _pokemonTeam = new List<Pokemon>();
+
+        //add to list (limit pokemon to 6)
+        public void AddPokemonToTeam(Pokemon pokemon)
+        {
+            _pokemonTeam.Add(pokemon);
+        }
+        //get list
+        public List<Pokemon> GetPokemonTeam()
+        {
+            return _pokemonTeam;
+        }
+        //get one pokemon
+        //public Pokemon GetPokemonByTeamPosition(int teamPosition)
+        //{
+        //    Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+        //    return pokemon;
+        //}
+
+        //Alternative to ^^above?
+        //Helper method (GetByTeamPosition)
+        public Pokemon GetPokemonByPosition(int teamPosition)
+        {
+            foreach (Pokemon pokemon in _pokemonTeam)
+            {
+                if (pokemon.TeamPosition == teamPosition)
+                {
+                    return pokemon;
+                }
+            }
+            return null;
+        }
+
+
+
+
+        //update pokemon
+        public void UpdatePokemonByTeamPosition(int teamPosition, Pokemon newPokemon)
+        {
+            //Find
+            Pokemon oldPokemon = GetPokemonByPosition(teamPosition);
+
+            //Update
+            if (oldPokemon != null)
+            {
+                Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+                pokemon.PokemonSpeciesName = newPokemon.PokemonSpeciesName;
+                pokemon.PokemonNickName = newPokemon.PokemonNickName;
+                pokemon.Level = newPokemon.Level;
+                Pokemon.PokemonPrimaryType = newPokemon.PokemonPrimaryType;
+                Pokemon.PokemonSecondaryType = newPokemon.PokemonSecondaryType;
+                pokemon.MoveOne = newPokemon.MoveOne;
+                pokemon.MoveTwo = newPokemon.MoveTwo;
+                pokemon.MoveThree = newPokemon.MoveThree;
+                pokemon.MoveFour = newPokemon.MoveFour;
+            }
+        }
+
+        public void UpdatePokemonByNickName(string nickName, Pokemon newPokemon)
+        {
+            foreach(Pokemon pokemon in _pokemonTeam)
+            {
+                if(nickName == pokemon.PokemonNickName)
+                {
+                    p = new Pokemon;
+                }
+            }
+        }
+
+        //remove pokemon
+        public void RemovePokemonFromTeam(int teamPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            _pokemonTeam.Remove(pokemon);
+        }
+    }
+}
